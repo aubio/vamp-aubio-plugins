50179e64abae6081f0109c524eea2ec796759b54
Paul Brossier <piem@piem.org>
2016-07-21 23:02:08 +0200

    scripts/get_waf.sh: add script to fetch latest waf

---
+++ scripts/get_waf.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+set -e
+set -x
+
+WAFURL=https://waf.io/waf-1.9.1
+
+( which wget > /dev/null && wget -qO waf $WAFURL ) || ( which curl > /dev/null && curl $WAFURL > waf )
+
+chmod +x waf
