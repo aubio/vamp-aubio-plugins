b5dcc8a44e049efd8e6f2c2035618c21dd643046
Paul Brossier <piem@piem.org>
2016-07-22 05:33:28 +0200

    scripts/get_aubio.sh: use get_waf.sh, add windows options

--- scripts/get_aubio.sh
+++ scripts/get_aubio.sh
@@ -3,15 +3,17 @@
 set -e
 set -x
 
+which cl.exe && WAFOPTS='--msvc_version="msvc 12.0" --msvc_target="x86"'
+
 mkdir -p contrib
 pushd contrib
 git clone https://github.com/aubio/aubio aubio || ( pushd aubio; git pull; popd )
 
 #rm -rf aubio
 pushd aubio
-make getwaf
+./scripts/get_waf.sh
 #./waf distclean
-./waf configure --prefix=$PWD/../aubio-dist --disable-atlas
+./waf configure --prefix=$PWD/../aubio-dist --disable-atlas $WAFOPTS
 ./waf build -v
 ./waf install -v
 popd
