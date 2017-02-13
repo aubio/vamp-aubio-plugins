86dc09bfb31e401c9a78b09eea045590179d6406
Paul Brossier <piem@piem.org>
2017-02-13 17:35:29 +0100

    scripts/get_aubio.sh: compile against lasts release when VAMP_AUBIO_VERSION_STATUS is empty

--- scripts/get_aubio.sh
+++ scripts/get_aubio.sh
@@ -3,12 +3,26 @@
 set -e
 set -x
 
+source VERSION
+
 mkdir -p contrib
 pushd contrib
-git clone https://github.com/aubio/aubio aubio || ( pushd aubio; git pull; popd )
+if [ -n "$VAMP_AUBIO_VERSION_STATUS" ]
+then
+  git clone https://github.com/aubio/aubio aubio || ( pushd aubio; git pull; popd )
+  pushd aubio
+else
+  AUBIO_VERSION=0.4.4
+  echo "using release 0.4.4"
+  tarball=aubio-$AUBIO_VERSION.tar.bz2
+  [ -f $tarball ] || wget https://aubio.org/pub/$tarball
+  [ -f $tarball.asc ] || wget https://aubio.org/pub/$tarball.asc
+  gpg --verify $tarball.asc $tarball
+  rm -rf ${tarball%%.tar.bz2} && tar xf $tarball
+  pushd ${tarball%%.tar.bz2}
+fi
 
 #rm -rf aubio
-pushd aubio
 ./scripts/get_waf.sh
 #./waf distclean
 ./waf configure --prefix=$PWD/../aubio-dist $WAFOPTS
