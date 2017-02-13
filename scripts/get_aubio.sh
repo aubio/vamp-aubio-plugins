65d39ccd29ad0a4de426667688c5372074a97c27
Paul Brossier <piem@piem.org>
2017-02-14 00:47:17 +0100

    scripts/get_aubio.sh: disable external dependencies, use curl

--- scripts/get_aubio.sh
+++ scripts/get_aubio.sh
@@ -5,6 +5,9 @@ set -x
 
 source $PWD/VERSION
 
+# external dependencies are not required for vamp
+WAFOPTS="--disable-samplerate --disable-avcodec --disable-sndfile --disable-jack"
+
 mkdir -p contrib
 pushd contrib
 if [ -n "$VAMP_AUBIO_VERSION_STATUS" ]
@@ -14,8 +17,8 @@ then
 else
   echo "using release 0.4.4"
   tarball=aubio-$AUBIO_VERSION.tar.bz2
-  [ -f $tarball ] || wget https://aubio.org/pub/$tarball
-  [ -f $tarball.asc ] || wget https://aubio.org/pub/$tarball.asc
+  [ -f $tarball ] || curl -O https://aubio.org/pub/$tarball
+  [ -f $tarball.asc ] || curl -O https://aubio.org/pub/$tarball.asc
   gpg --verify $tarball.asc $tarball
   rm -rf ${tarball%%.tar.bz2} && tar xf $tarball
   pushd ${tarball%%.tar.bz2}
